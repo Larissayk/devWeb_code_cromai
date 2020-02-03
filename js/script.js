099e477b07153ebe92aa84bc3d129cec6d035346
Larissayk <lari.yukie@gmail.com>
2020-02-02 23:53:37 -0300

    teste fim

--- js/script.js
+++ js/script.js
@@ -1,41 +1,4 @@
-// Sending the values
-const Form = document.querySelector("form");
-
-const formCalculator = Form.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const cat_op = parseInt(document.querySelector("#cateto_op").value);
-  const cat_adj = parseInt(document.querySelector("#cateto_adj").value);
-
-  const calculatorValues = {
-    cat_op,
-    cat_adj
-  };
-  console.log(calculatorValues);
-
-  calculateHip(calculatorValues);
-});
-
-// function formValidation() {
-//   const cat_oposto = document.getElementById("cateto_op");
-//   const cat_adjascente = document.getElementById("cateto_adj");
-
-//   const forms = document.getElementsByClassName("needs-validation");
-//   // Loop over them and prevent submission
-//   const validation = Array.prototype.filter.call(forms, function(form) {
-//     form.addEventListener("submit", function(event) {
-//       if (form.checkValidity() === false) {
-//         event.preventDefault();
-//         event.stopPropagation();
-//       }
-//       form.classList.add("was-validated");
-//     });
-//   });
-// }
-
-
-
-// // Form Validation
+//Form Validation MDBootstrap
 (function() {
   "use strict";
   window.addEventListener(
@@ -43,18 +6,18 @@ const formCalculator = Form.addEventListener("submit", event => {
     function() {
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
       const forms = document.getElementsByClassName("needs-validation");
-      const cat_oposto = document.getElementById("cateto_op");
-      const cat_adjascente = document.getElementById("cateto_adj");
+      const Form = document.querySelector("form");
       // Loop over them and prevent submission
       const validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener(
+        Form.addEventListener(
           "submit",
           function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add("was-validated");
+            // if (Form.checkValidity() === false) {
+            //   console.log("invalido");
+            //   event.preventDefault();
+            //   event.stopPropagation();
+            // }
+            Form.classList.add("was-validated");
           },
           false
         );
@@ -64,7 +27,35 @@ const formCalculator = Form.addEventListener("submit", event => {
   );
 })();
 
-//Calculate Hipotenuse
+// Sending the values to the API if form is valid
+const Form = document.querySelector("form");
+const formCalculator = Form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const cat_op = parseInt(document.querySelector("#cateto_op").value);
+  const cat_adj = parseInt(document.querySelector("#cateto_adj").value);
+
+  const catOP_email = document.querySelector("#valorCatOp");
+  catOP_email.innerText = cat_op;
+  const catAD_email = document.querySelector("#valorCatAd");
+  catAD_email.innerText = cat_adj;
+
+  const calculatorValues = {
+    cat_op,
+    cat_adj
+  };
+  console.log(calculatorValues);
+
+  if (Form.checkValidity() == false) {
+    event.preventDefault();
+    event.stopPropagation();
+  } else {
+    calculateHip(calculatorValues);
+    $("#modalPush").modal("show");
+  }
+});
+
+//Calculate Hypotenuse
 const calculateHip = calculatorValues => {
   axios
     .post("https://atlas-231814.appspot.com/calcula", calculatorValues)
@@ -74,9 +65,18 @@ const calculateHip = calculatorValues => {
       console.log(`O valor do cálculo da hipotenusa é:`, hypotenuse);
       // Show result
       document.querySelector("#hipotenusa").value = hypotenuse;
+      const hip = document.querySelector("#resultHip");
+      hip.innerText = hypotenuse;
+
+      console.log(hip);
     })
     .catch(error => console.error(error));
 };
 
+function clearInput() {
+  console.log("limpar");
+  Form.reset();
+  Form.classList.remove("was-validated");
+}
 
 
